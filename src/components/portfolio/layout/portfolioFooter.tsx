5825af58798917006f1f5422b9b57e6c423cf48c
KTG <xorb269@naver.com>
2024-04-16 18:14:15 +0900

    Temp: 노트북 to 데스크탑 옮김

--- src/components/portfolio/layout/portfolioFooter.tsx
+++ src/components/portfolio/layout/portfolioFooter.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import { Wrapper } from "../../../styles/layouts/portfolioFooter"
 
 const Footer = () => {
-  return <></>
+  return <Wrapper></Wrapper>
 }
 
 export default Footer
