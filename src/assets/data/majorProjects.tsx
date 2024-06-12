a3b6093caf67dd4206af4d18c35a39b88d8e08b7
KTG <xorb269@naver.com>
2024-06-12 21:52:55 +0900

    Feat: trip together 프로젝트 추가

--- src/assets/data/majorProjects.tsx
+++ src/assets/data/majorProjects.tsx
@@ -44,8 +44,8 @@ const majors: major[] = [
     id: major_id++,
     title: "SAMSUNG Signature",
     size: {
-      period: "7주 2024.01.03 ~ 2024.02.16",
-      personnel: "6명 Backend 3, Frontend 3",
+      period: "6주 / 2024.04.08~2024.05.20",
+      personnel: "6명 / Backend 3, Frontend 3",
     },
     pages: [
       {
@@ -104,11 +104,11 @@ const majors: major[] = [
         title: "FE",
         skills: [
           "Typescript",
-          "React-native",
+          "React Native",
           "Redux",
           "Android Studio",
-          "Styped-components",
-          "Tanstack-query",
+          "Styped Components",
+          "Tanstack Query",
           "MSW",
         ],
       },
@@ -151,6 +151,115 @@ const majors: major[] = [
       },
     ],
     colors: ["#1428A0", "#94c3d7"],
+  },
+  {
+    id: major_id++,
+    title: "Trip Together",
+    size: {
+      period: "7주 / 2024.02.19~2024.04.05",
+      personnel: "6명 / Backend 4, Frontend 2",
+    },
+    pages: [
+      {
+        src: "images/projects/2-1.png",
+        alt: "샘플이미지",
+      },
+    ],
+    abstract: "은행에서 출시한 백패커를 위한 슈퍼앱",
+    descript: [
+      "'Trip-Together'는 은행에서 출시한 백패커들을 위한 여행 애플리케이션으로, 간편 동행 모집(번개 모임)과 은행 API를 통한 환전, 결제, 정산 기능을 갖춘 '슈퍼앱'입니다.",
+      "'은행 기능'과 '소셜 로그인' 구현을 위해 저희는 'Trip Together'와 'Twinkle Bank' 두 개의 서버를 개발하였습니다.",
+    ],
+    readme:
+      "https://quickest-blue-4f2.notion.site/Trip-Together-0950db5f7ceb491899c5b5b2fadf525f?pvs=4",
+    features: [
+      {
+        title: "소셜 로그인",
+        content:
+          "Twinkle Bank의 계정으로 Trip Together에서 간편하게 로그인할 수 있습니다.",
+      },
+      {
+        title: "핀번호 관리",
+        content:
+          "최초 로그인을 통해 Twinkle Bank로부터 인증을 받은 이후로는 Trip Together의 핀번호를 통해 Twinkle Bank로부터 인증토큰을 받아올 수 있습니다.",
+      },
+      {
+        title: "1원 인증 및 계좌 연동",
+        content:
+          "Trip-Together에서 환전시 사용할 은행계좌를 연동하기 위해 사용자는 1원을 해당 계좌로 송금받고, 송금자명을 입력하여 인증을 받을 수 있습니다.",
+      },
+      {
+        title: "현 위치, 장소 기반 모임",
+        content:
+          "사용자의 위치, 장소를 기반으로 번개 모임을 모집, 참여할 수 있습니다.",
+      },
+      {
+        title: "모임 채팅",
+        content: "모임 개설 시 참여자들을 위한 채팅방이 생성됩니다.",
+      },
+      {
+        title: "결제 내역 기반 더치페이",
+        content: "실제 결제 내역을 기반으로 번개 모임 정산을 할 수 있습니다.",
+      },
+      {
+        title: "여행 계획 및 예산 책정",
+        content:
+          "사용자들의 실제 결제 내역이 적용된 여행지의 인당 평균 소비 금액을 통해 여행 예산을 책정할 수 있습니다.",
+      },
+      {
+        title: "간편 환전",
+        content:
+          "앱 내 지갑을 통해 “원화 <-> 해외 통화”로의 자유로운 환전이 가능합니다.",
+      },
+      {
+        title: "바코드 결제",
+        content:
+          "사용자들은 여행지에 대한 결제를 앱 내에서 간편 결제를 통해 진행합니다.",
+      },
+    ],
+    fields: [
+      {
+        title: "FE",
+        skills: [
+          "Typescript",
+          "React Native",
+          "React",
+          "Redux",
+          "Styled Components",
+          "Axios",
+          "StompJS",
+          "Flipper",
+        ],
+      },
+      {
+        title: "BE",
+        skills: [
+          "Java",
+          "Spring Boot",
+          "JPA",
+          "QueryDSL",
+          "MySQL",
+          "MongoDB",
+          "Redis",
+          "Apachekafka",
+        ],
+      },
+      {
+        title: "Infra",
+        skills: ["Nginx", "Jenkins", "Docker", "AWS EC2", "Gitlab"],
+      },
+    ],
+    urls: [
+      {
+        title: "Github",
+        link: "https://github.com/SSAFY-A309/Trip-Together",
+      },
+      {
+        title: "notion",
+        link: "https://sirlyun.notion.site/Trip-Together-99f0a5baec0a498196aaf2f69766758b?pvs=4",
+      },
+    ],
+    colors: ["#56E5FF", "#f0f0f0"],
   },
 ]
 
