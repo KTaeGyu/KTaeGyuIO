8e0d7ef9f4b030f6f6c898820b03fa5ea5a27252
KTaeGyu <xorb269@naver.com>
2024-11-17 20:34:52 +0900

    상수 추가

---
+++ src/components/chichi/author/PostModal.constant.ts
@@ -0,0 +1,14 @@
+const IMG_PATHS = [
+  "/images/chichi/bell.png",
+  "/images/chichi/candycane.png",
+  "/images/chichi/chinaholly.png",
+  "/images/chichi/cookiehouse.png",
+  "/images/chichi/ornament.png",
+  "/images/chichi/santahat.png",
+  "/images/chichi/treedecoration.png",
+  "/images/chichi/wreath.png",
+]
+
+const C = { IMG_PATHS }
+
+export default C
