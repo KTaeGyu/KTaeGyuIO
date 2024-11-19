8ba6387a7f42df2f0088396468c0620c5f06ee2d
KTaeGyu <xorb269@naver.com>
2024-11-19 20:15:29 +0900

    Fix: 빌드 에러 posted 수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.createPages = async ({ graphql, actions }) => {
           contentful_id
           name
           username
-          posted {
+          post {
             id
             title
             imgPath
