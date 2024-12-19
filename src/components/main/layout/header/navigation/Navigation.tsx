ecf32f2d32a3f73f24b8fb3753156712abc149f2
KTaeGyu <xorb269@naver.com>
2024-12-19 09:07:57 +0900

    Feat:  네비게이션 기능

--- src/components/main/layout/header/navigation/Navigation.tsx
+++ src/components/main/layout/header/navigation/Navigation.tsx
@@ -1,13 +1,30 @@
-import React from "react"
+import { navigate } from "gatsby"
+import React, { useEffect, useState } from "react"
 import S from "./Navigation.styles"
 
 export default function Navigation() {
+  const [canBack, setCanBack] = useState(false)
+  const [canForward, setCanForward] = useState(false)
+
+  useEffect(() => {
+    const onPopState = () => {
+      setCanBack(window.history.state !== null)
+      setCanForward(window.history.length > 2)
+    }
+    // 초기 실행
+    onPopState()
+    // 등록
+    window.addEventListener("popstate", onPopState)
+    // 제거
+    return () => window.removeEventListener("popstate", onPopState)
+  }, [])
+
   return (
     <S.Container>
-      <S.Button>
+      <S.Button onClick={() => navigate(-1)} disabled={!canBack}>
         <S.BackIcon />
       </S.Button>
-      <S.Button>
+      <S.Button onClick={() => history.forward()} disabled={!canForward}>
         <S.ForwardIcon />
       </S.Button>
     </S.Container>
